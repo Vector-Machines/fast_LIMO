b1b82c52c265802a3b14083a6cb32bc129f06113
Elmar <elmar@vectormachines.nl>
2025-07-07 20:52:02 +0200

    Update point time extraction for LIVOX timestamps to use absolute time directly

--- include/fast_limo/Modules/Localizer.cpp
+++ include/fast_limo/Modules/Localizer.cpp
@@ -792,9 +792,9 @@
                 
                 point_time_cmp = [](const PointType& p1, const PointType& p2)
                 { return p1.timestamp < p2.timestamp; };
-                extract_point_time = [&sweep_ref_time, &end_of_sweep](PointType& pt)
-                {   if (end_of_sweep) return sweep_ref_time - pt.timestamp * 1e-9f; 
-                    else return sweep_ref_time + pt.timestamp * 1e-9f; };
+                // LIVOX timestamp is now in seconds (absolute time), use directly
+                extract_point_time = [](PointType& pt)
+                { return pt.timestamp; };
             } else {
                 std::cout << "-------------------------------------------------------------------\n";
                 std::cout << "FAST_LIMO::FATAL ERROR: LiDAR sensor type unknown or not specified!\n";
